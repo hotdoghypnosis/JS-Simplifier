d238d80afb320fd70854756b2dd71e6cc0ff4116
hotdoghypnosis <56368007+hotdoghypnosis@users.noreply.github.com>
2020-02-07 15:38:57 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+/*
 
+  first commit: the following is the first uploaded variant
+  ! Note: do not attempt to load twice unless it fails to preload
+  
+*/
+
+// sidebar
+
+container = {
+  sidebar: {
+    template (width, height, instance){
+    
+    }
+  }
+}
